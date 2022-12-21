2f8304f844a7204e186a63cca3a2782f64bdfa9a
sujin-park0607 <goweg0607@gmail.com>
2022-12-21 14:45:10 +0900

    BasicController 수정

--- src/main/java/com/likelion/finalproject/controller/BasicController.java
+++ src/main/java/com/likelion/finalproject/controller/BasicController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BasicController {
 
     @GetMapping("/hello")
-    public ResponseEntity<String> hello(){
-        return ResponseEntity.ok().body("welcome");
+    public String hello(){
+        return "hello";
     }
 }
